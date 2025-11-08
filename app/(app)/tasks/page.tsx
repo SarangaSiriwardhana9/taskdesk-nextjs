cf46fdc574942205f94de9e0cd49bc24f15c78ed
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2025-11-09 02:38:54 +0530

    fix: fix task page ui

--- app/(app)/tasks/page.tsx
+++ app/(app)/tasks/page.tsx
@@ -184,7 +184,7 @@ export default function TasksPage() {
   }
 
   return (
-    <main className="min-h-screen bg-background pt-20 ">
+    <main className="min-h-screen bg-background pt-10 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="space-y-12">
           <TasksPageHeader tasks={tasks} />
